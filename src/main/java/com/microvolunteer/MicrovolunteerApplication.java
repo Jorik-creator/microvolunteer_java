79c48a76063a44a5cde54e43ff5fd7ad3f2370d7
ypoli <y.polishchuk05@gmail.com>
2025-04-19 23:57:26 +0200

    Ініціалізація проекту

---
+++ src/main/java/com/microvolunteer/MicrovolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.microvolunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrovolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrovolunteerApplication.class, args);
+	}
+
+}
